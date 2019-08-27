49fe737ad80e4735511411c68a04e645e7b00715
Creed, Jordan H <Jordan.H.Creed@moffitt.org>
2019-08-27 11:06:05 -0400

    document example data

---
+++ R/data-example250.R
@@ -0,0 +1,16 @@
+#' Example mediation data
+#'
+#' This data was randomly generated and based off the example data provided in the SAS macro <https://www.hsph.harvard.edu/tyler-vanderweele/tools-and-tutorials/>
+#'
+#' @format A data table with 250 rows and 6 variables:
+#' \describe{
+#' \item{id}{Numeric identifier for each observation (1:250)}
+#' \item{x}{Random binary variable (0,1)}
+#' \item{m}{Random binary variable (0,1)}
+#' \item{y}{Random continuous variable (-3.04 to 3.54)}
+#' \item{cens}{Random binary variable (0,1)}
+#' \item{c}{Random continuous variable (-3.11 to 2.52)}
+#' }
+#' @examples
+#' example250
+"example250"
